41cf0007d68471426a307de4ded2cb23a3676de7
Panagiotis Panagoulias <ppanagoulias82@gmail.com>
2024-09-22 13:33:29 +0300

    Create CLI
    Create a command line client as a separate application

--- Makefile
+++ Makefile
@@ -1,12 +1,21 @@
 .DEFAULT_GOAL := goapp
 
 .PHONY: all
-all: clean goapp
+all: clean goapp cli test
 
 .PHONY: goapp
 goapp:
 	mkdir -p bin
-	go build -o bin ./...
+	go build -o bin ./cmd/server
+
+.PHONY: cli
+cli:
+	mkdir -p bin
+	go build -o bin ./cmd/cli
+
+.PHONY: test
+test:
+	go test ./...
 
 .PHONY: clean
 clean:
---
+++ cmd/cli/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"goapp/internal/pkg/watcher"
+	"log"
+	"net"
+	"net/url"
+	"os"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	// Flags
+	n := flag.Int("n", 0, "Specify the number of parallel connections")
+	host := flag.String("host", "localhost", "(optional) Specify the host to connect to")
+	port := flag.String("port", "8080", "(optional) Specify the port to connect to")
+	path := flag.String("path", "/goapp/ws", "(optional) Websocket path")
+
+	flag.Parse()
+
+	// Check wether a valid value provided for n
+	if *n <= 0 {
+		log.Println("Error: The -n flag has been ommited or an invalid value provided")
+		flag.Usage() // Prints usage information
+		os.Exit(1)   // Exit with non-zero status to indicate an error
+	}
+
+	// Prepare URL
+	hostPort := net.JoinHostPort(*host, *port)
+	wsUrl := url.URL{
+		Scheme: "ws",
+		Host:   hostPort,
+		Path:   *path,
+	}
+
+	var wg sync.WaitGroup
+
+	// Start n concurrent websocket connections
+	for i := 0; i < *n; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+
+			conn, _, err := websocket.DefaultDialer.Dial(wsUrl.String(), nil)
+			if err != nil {
+				log.Printf("Failed to connect to thewebsocket server, connection id: %d, error: %v\n", id, err)
+				return
+			}
+			defer conn.Close()
+
+			// Read and handle messages received by the server on the websocket connection
+			for {
+				_, msg, err := conn.ReadMessage()
+				if err != nil {
+					log.Printf("Error reading message: %v\n", err)
+					break
+				}
+
+				var receivedMsg watcher.Counter
+				if err := json.Unmarshal(msg, &receivedMsg); err != nil {
+					log.Printf("Error unmarshalling message: %v\n", err)
+					continue
+				}
+
+				// Print message in a specific format (e.g.: [conn #0] iteration: 1, value: 66D53ED788)
+				fmt.Printf("[conn #%d] iteration: %d, value: %s\n", id, receivedMsg.Iteration, receivedMsg.Value)
+			}
+
+		}(i)
+	}
+
+	// Using a wait group to indefinitely prevent the function from terminating until a stop signal is triggered by the user
+	wg.Wait()
+}
